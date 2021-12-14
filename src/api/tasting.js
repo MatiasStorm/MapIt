c446171a584b9a4d3dd95e97245947ce5e966d4b
MatiasStorm <mat-435@hotmail.com>
2021-12-14 13:35:24 +0100

    Include user id in tasting post

--- src/api/tasting.js
+++ src/api/tasting.js
@@ -1,12 +1,14 @@
 const { Tasting } = require("../models");
 const router = require("express").Router();
+const { authenticateToken } = require("../auth");
 
 router.get("/:id", async (req, res) => {
     res.json({ name: "Test" });
 });
 
-router.post("/", async (req, res) => {
+router.post("/", authenticateToken, async (req, res) => {
     try {
+        req.body.userId = req.user.id;
         const tasting = await Tasting.create(req.body);
         return res.status(201).json(tasting);
     }
